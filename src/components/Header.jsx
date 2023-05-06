2898a57faf575f5eba9dbfe967590a03be26c47e
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-06 13:10:06 +0530

    navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+export default function Header(){
+    return (
+        <nav>
+            <h1>TechStar</h1>
+            <main>
+                <Link to= {"/"}>Home</Link>
+                <Link to= {"/contact"}>Contact</Link>
+                <Link to= {"/#about"}>About</Link>
+                <Link to= {"/#brands"}>Brands</Link>
+                <Link to= {"/services"}>Services</Link>
+                <Link to= {"/"}>Home</Link>
+            </main>
+        </nav>
+    )
+}
