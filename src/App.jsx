bb5ed4fc77191db40951a0ee135312b3a5d9d7bf
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-08 07:27:21 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Header from "./components/Header.jsx"
 import Home from "./components/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import Contact from "./components/Contact.jsx";
-
+import Services from "./components/Services.jsx";
 
 import "./styles/App.scss"
 import "./styles/Header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
 import "./styles/contact.scss"
+import "./styles/mediaquery.scss"
+
 
 function App() {
   
@@ -22,7 +24,7 @@ function App() {
       <Routes>  
         <Route path= "/" element={<Home />}/>
         <Route path= "/contact" element={<Contact />}/>
-
+        <Route path= "/services" element={<Services />}/>
       </Routes>
       <Footer />
 
