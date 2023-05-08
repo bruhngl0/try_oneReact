f698c84af087622c98c2e5a03d47285e800a7e46
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-08 05:47:54 +0530

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header.jsx"
 import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
+import Contact from "./components/Contact.jsx";
+
 
 import "./styles/App.scss"
 import "./styles/Header.scss"
 import "./styles/home.scss"
+import "./styles/footer.scss"
+import "./styles/contact.scss"
 
 function App() {
   
@@ -16,7 +21,10 @@ function App() {
       <Header />
       <Routes>  
         <Route path= "/" element={<Home />}/>
+        <Route path= "/contact" element={<Contact />}/>
+
       </Routes>
+      <Footer />
 
     </Router>
   )
